27e77fb2c5f6b7e04bae4410897b35c204ba6a44
Marc Schneider <marc@shnei.de>
2021-04-18 00:18:01 +0200

    Slack with cnalle scurity

--- backend/slack.py
+++ backend/slack.py
@@ -3,24 +3,39 @@ from index import app
 from pydantic import BaseModel
 
 class SlackQuestion(BaseModel):
-    token: str
-    team_id: str
-    team_domain: str
-    enterprise_id: str
-    enterprise_name: str
-    channel_id: str
-    channel_name: str
-    user_id: str
-    user_name: str
+    # token: str
+    # team_id: str
+    # team_domain: str
+    # enterprise_id: str
+    # enterprise_name: str
+    # channel_id: str
+    # channel_name: str
+    # user_id: str
+    # user_name: str
     command: str
     text: str
-    response_url: str
-    trigger_id: str
-    api_app_id: str
+    # response_url: str
+    # trigger_id: str
+    # api_app_id: str
 
 @app.post("/api/questionFromSlack")
 async def question_by_slack(request: Request):
-    print("Test")
-    print(request)
-    return "Hello world!"
+    body = await request.form()
+    print(body)
+    channel_name = body["channel_name"]
+    question = body["text"]
+    if channel_name == "smashers":
+        return {
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"*Your Question:* {question}"
+                    }
+                }
+            ]
+        }
+    else:
+        return f"You can't use this bot from the {channel_name} channel!!! :=("
 
