f293dac15ff727e4ddf43d607753c7ccef978761
rosenauer <rosenauer@fs.tum.de>
2021-04-18 00:21:39 +0200

    finishing hard_coding

--- backend/hardcode.py
+++ backend/hardcode.py
@@ -1,11 +1,33 @@
 import pandas as pd
-
+from docx import Document
 
 def get_lst():
     df = pd.read_excel('backend/Sample_Questions_Compliance.xls')
     lst = []
+    
     for index, row in df.iterrows():
         lst.append((row['Model Question'], row['Additional Tags / Synonyms for questions (from QnA Chatbot)'],row['Model Answer']))
+
+    wordDoc = Document('backend/Sample_Questions_Legal.docx')
+
+    for table in wordDoc.tables:
+        for row in table.rows:
+            print('')
+            print('')
+            print('')
+            sub = [0,'',0]
+            i = 0
+            for cell in row.cells:
+                try:
+                    int(cell.text)
+                except Exception as e:
+                    if i in [0, 2]:
+                        sub[i] = cell.text
+                    if cell.text != '':
+                        print(cell.text)
+                        i = i + 1
+            lst.append(tuple(sub))
+
     return lst
 
 def str_in_lst(question, lst):
