f83667a0e49a79fd50579e1015f45624296bb7ce
Simon Bohnen <simon.bohnen@tum.de>
2021-04-17 12:30:00 +0200

    Adapt example context

--- backend/gptapi.py
+++ backend/gptapi.py
@@ -19,6 +19,40 @@ header_contenttype = "application/json"
 search_model = "ada"
 completion_model = "curie"
 
+examples_context = """6.6.1.3	Decent Working Time
+
+Violations of working time through the practices of state and non-state actors my limit the right of a person to 
+decent working hours. Siemens may face several operational, financial, legal and reputational risks where these 
+standards aren't met. Specific risks may arise from potential violations of working hours regulations 
+project business on the part of suppliers, subcontractors or other businesses involved in the project.
+Definition of the Issue
+The right to decent working time is enshrined in Article 7 of the International Covenant on Economic, Social and 
+Cultural Rights (ICESCR). Specifically, workers are entitled to the right to rest, leisure, reasonable limitation 
+of working hours and adequate holiday allowances with pay. These conditions are linked to basic human 
+rights including the right to an adequate standard of living (including adequate food, clothing and housing), the 
+right to physical and mental health and the right to life.
+Numerous ILO conventions provide for more detailed provisions on decent working hours laid down in the 
+ICESCR. ILO convention 1 stipulates that generally working time should not exceed 48 hours per week, or 
+eight hours a day, with at least one day off in every seven. ILO convention 30 states a working day cannot 
+exceed ten hours. Furthermore, ILO convention 1 also states that where overtime work is allowed and required, 
+the maximum number of additional hours must be fixed, and overtime must be compensated at a rate of 
+no less than 25% above the normal rate of pay.
+"""
+examples = [
+    [
+        "Where is the right to decent working hours defined?",
+        "In Article 7 of the International Covenant on Economic, Social and Cultural Rights (ICESCR)."
+    ],
+    [
+        "How should overtime be compensated?",
+        "At least 25% above the normal rate of pay."
+    ],
+    [
+        "What risks does Siemens face when the decent working hour standards are not met?",
+        "Siemens risks being faced with operational, financial, legal and reputational risks."
+    ],
+]
+
 
 def get_answer(question: str, paragraphs: List[str]):
     # question = "Is a payment under duress deemed an act of corruption?"
@@ -34,8 +68,8 @@ def get_answer(question: str, paragraphs: List[str]):
             "question": question,
             "search_model": search_model,
             "model": completion_model,
-            "examples_context": "A facilitation payment is the payment of a relatively small amount of money or the granting of any other benefit to usually low-ranking government officials, for their own personal benefit, with the aim of speeding up the performance of an official act to which the person making the payment/granting the benefit is entitled. This means that the government entity concerned would perform the official act in the same form and in any event, without payment to the government official (including, for example, granting approval where all preconditions are satisfied).",
-            "examples": [["Who does a facilitation payment go to?", "Low-ranking government officials."]],
+            "examples_context": examples_context,
+            "examples": examples,
             "max_tokens": 40,
             "stop": ["\n", "<|endoftext|>"],
             "temperature": 0.2
