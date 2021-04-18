2055bd195850a291f3a09fcc809612104add10ce
rosenauer <rosenauer@fs.tum.de>
2021-04-18 13:19:10 +0200

    Test

--- backend/hardcode.py
+++ backend/hardcode.py
@@ -2,13 +2,13 @@ import pandas as pd
 from docx import Document
 
 def get_lst():
-    df = pd.read_excel('Sample_Questions_Compliance.xls')
+    df = pd.read_excel('backend/Sample_Questions_Compliance.xls')
     lst = []
     
     for index, row in df.iterrows():
         lst.append((row['Model Question'], row['Additional Tags / Synonyms for questions (from QnA Chatbot)'],row['Model Answer']))
 
-    wordDoc = Document('Sample_Questions_Legal.docx')
+    wordDoc = Document('backend/Sample_Questions_Legal.docx')
 
     for table in wordDoc.tables:
         for row in table.rows:
@@ -18,8 +18,11 @@ def get_lst():
                 try:
                     int(cell.text)
                 except Exception as e:
-                    if i in [0, 2]:
-                        sub[i] = cell.text
+                    if i in [0, 1]:
+                        if i == 0:
+                            sub[i] = cell.text
+                        if i == 1:
+                            sub[i+1] = cell.text
                     if cell.text != '':
                         i = i + 1
             lst.append(tuple(sub))
@@ -39,4 +42,4 @@ def str_in_lst(quest, lst):
 
     
     
-get_lst()
+print(get_lst())
